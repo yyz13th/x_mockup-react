b3162b8540582325a34f84a3865d888e5cf50ad7
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-10-24 15:25:11 +0300

    posts on submit

--- src/components/post-add-form/post-add.component.jsx
+++ src/components/post-add-form/post-add.component.jsx
@@ -1,20 +1,44 @@
 import './post-add.styles.css';
+import { Component } from 'react';
+export default class PostAddForm extends Component {
 
-const PostAddForm = ({onAdd}) => {
-    return(
-        <form className="bottom-panel d-flex">
-            <input 
-                type="text"
-                placeholder="Whats on your mind?"
-                className="form-control new-post-label" 
-                />
-            <button type="button" className="btn btn-outline-secondary"
-            onClick={()=> onAdd('text')}>
-                Add Post
-            </button>
-        </form>
-    )
+    constructor(props){
+        super(props);
+        this.state = {
+            text: ''
+        }
+        this.onValueChange = this.onValueChange.bind(this);
+        
+    }
+    onValueChange = (e) => {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.text);
+        this.setState({
+            text: ''
+        })
+    }
+    render() {
+        return(
+            <form className="bottom-panel d-flex"
+                onSubmit = {this.onSubmit.bind(this)}>
+                <input 
+                    type="text"
+                    placeholder="Whats on your mind?"
+                    className="form-control new-post-label" 
+                    onChange={this.onValueChange}
+                    value={this.state.text}
+                    />
+                <button type="submit" className="btn btn-outline-secondary">
+                    Add Post
+                </button>
+            </form>
+        )
+    }
+    
 }
-
-export default PostAddForm;
-
